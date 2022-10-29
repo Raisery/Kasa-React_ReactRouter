c3e6d2ce6c86f9361bfec6feabcc0d13b10f2c39
Lucas GERARD <lucas1.gerard@gmail.com>
2022-10-29 18:16:52 +0200

    add 404 page

--- src/pages/Error404/Error404.jsx
+++ src/pages/Error404/Error404.jsx
@@ -1,7 +1,12 @@
+import "../../css/error404.css"
+import { Link } from "react-router-dom";
+
 function Error() {
     return (
-        <div>
-            <h1>Oups 🙈 Cette page n'existe pas : ERROR 404</h1>
+        <div className="error_404">
+            <h1>404</h1>
+            <h2>Oups! La page que vous demandez n'existe pas</h2>
+            <Link to='/'>Retourner sur la page d'accueil</Link>
         </div>
     )
 }
